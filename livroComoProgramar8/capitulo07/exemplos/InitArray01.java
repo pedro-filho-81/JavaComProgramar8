f91df8cb490a6bd66def2e7e9ba7cad48bbc2967
Pedro Filho <psfilho81@gmail.com>
2024-05-10 18:36:56 -0300

    Update InitArray01.java

--- livroComoProgramar8/capitulo07/exemplos/InitArray01.java
+++ livroComoProgramar8/capitulo07/exemplos/InitArray01.java
@@ -1,17 +1,17 @@
 public class InitArray01 {
    /*
-    * Inicializando os elementos do array com valores padrão como zeros
+    * Inicializando os elementos do array com valores padrão de zeros
     */
    public static void main(String[] args) {
       // declare and create an array to 10 element
       int[] array = new int[10];
-      // array title
+      // array header
       System.out.printf("%s%8s%n", "Index", "Value");
 
-      // for loop for initialize array and print result
+      // for loop to initialize array and print result
       for (int counter = 0; counter < 10; counter++) {
-         // print result
+         // print result index and zeros with element
          System.out.printf("%5s%8s%n", counter, array[counter]);
-      }
-   }
-}
+      } // end for
+   } // end main
+} // end class
