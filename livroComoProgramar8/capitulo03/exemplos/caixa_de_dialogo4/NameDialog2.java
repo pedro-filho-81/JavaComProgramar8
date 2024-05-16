b3d39464c19f0333899a018e992e005a42d6db29
Pedro Filho <psfilho81@gmail.com>
2024-05-16 18:42:32 -0300

    Create NameDialog2.java

---
+++ livroComoProgramar8/capitulo03/exemplos/caixa_de_dialogo4/NameDialog2.java
@@ -0,0 +1,18 @@
+package caixa_de_dialogo4;
+
+import javax.swing.JOptionPane;
+
+public class NameDialog2 {
+   public static void main(String[] args) {
+
+      // entrada de dados
+      // variável name recebe o nome informado pelo usuário
+      String name = JOptionPane.showInputDialog(null, "What is your name? ");
+
+      // variável message exiba mensagem de boas vindas
+      String message = String.format("Welcome, %s, to Java Programming!", name);
+
+      // exibe a mensagem comprimentando o usuário
+      JOptionPane.showMessageDialog(null, message);
+   } // end main
+} // end class
