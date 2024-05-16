6d9c3777d626d7ee5a69ca1502ead69e4ce5eed1
Pedro Filho <psfilho81@gmail.com>
2024-05-16 18:42:22 -0300

    Create Dialog1.java

---
+++ livroComoProgramar8/capitulo03/exemplos/caixa_de_dialogo4/Dialog1.java
@@ -0,0 +1,10 @@
+package caixa_de_dialogo4;
+
+// Usando JOptionPane para exibir múltiplas linhas em uma caixa de diálogo.
+import javax.swing.JOptionPane;
+
+public class Dialog1 {
+   public static void main(String[] args) {
+      JOptionPane.showMessageDialog(null, "Welcome to Java ");
+   }
+}
