671edf5d6ba00f5165a3072e28aea5f9f7a69221
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-21 15:45:19 -0300

    Create ClasseEmployee.java

---
+++ livroComoProgramar8/capitulo03/exercicios/employee/ClasseEmployee.java
@@ -0,0 +1,39 @@
+package capitulo03.exercicios.employee;
+
+/*
+ * 3.13 (Classe Employee) Crie uma classe chamada 
+ * Employee que inclua três variáveis de instância — 
+ * um primeiro nome (tipo String), um 
+ * sobrenome (tipo String) e um salário mensal 
+ * (double). Forneça um construtor que inicializa as 
+ * três variáveis de instância. Forneça um 
+ * método set e um get para cada variável de 
+ * instância. Se o salário mensal não for positivo, 
+ * não configure seu valor. Escreva um aplicativo de
+ * teste chamado EmployeeTest que demonstre as 
+ * capacidades da classe Employee. Crie dois objetos 
+ * Employee e exiba o salário anual
+ * de cada objeto. Então dê a cada Employee um 
+ * aumento de 10% e exiba novamente o salário anual 
+ * de cada Employee.
+ */
+public class ClasseEmployee {
+
+   // variáveis de instância
+   private String nome;
+   private String sobreNome;
+   private double salarioMes;
+
+   // construtor
+   public ClasseEmployee(String nome, String sobreNome, double salarioMes) {
+      this.nome = nome;
+      this.sobreNome = sobreNome;
+      this.salarioMes = salarioMes;
+   } // end construct
+
+   // cria método status
+   public void status() {
+      System.out.println("\nNovo Empregdo:");
+      System.out.printf("Nome: %s%nSobreNome: %s%nSalário R$: %.2f%n%n", this.nome, this.sobreNome, this.salarioMes);
+   } // end status
+} // end class
