a45a0b572f1645abc3105d3911fe802498a3d452
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-21 14:53:02 -0300

    Update ClasseInvoice.java

--- livroComoProgramar8/capitulo03/exercicios/invoice/ClasseInvoice.java
+++ livroComoProgramar8/capitulo03/exercicios/invoice/ClasseInvoice.java
@@ -27,5 +27,84 @@ package capitulo03.exercicios.invoice;
 public class ClasseInvoice {
 
    // Atributos
+   // Variáveis de instância
+   private String numFatura;
+   private String descricao;
+   private int quantiComprada;
+   private double preco;
+
+   // construtor da classe Invoice
+   public ClasseInvoice(String numFatura, String descricao, int quantiComprada, double preco) {
+      this.numFatura = numFatura;
+      this.descricao = descricao;
+
+      // this.quantiComprada = quantiComprada;
+      setQuantia(quantiComprada); // chama o método setQuantia
+
+      // this.preco = preco;
+      // chama o método setPreço
+      setPreco(preco);
+   } // end construtor
+
+   // getInvoiceAmount
+   public double getInvoiceAmount() {
+      return (quantiComprada * preco);
+   } // end getInvoiceAmount
+
+   // set fatura
+   public void setFatura(String numFatura) {
+      this.numFatura = numFatura;
+   } // end setFatura
+
+   // get fatura
+   public String getFatura() {
+      return numFatura;
+   } // end getFatura
+
+   // set descrição
+   public void setDescricao(String descricao) {
+      this.descricao = descricao;
+   } // end set descrição
+     // get descrição
+
+   public String getDiscricao() {
+      return descricao;
+   } // get end descrição
+
+   // set Quantia
+   public void setQuantia(int quantiComprada) {
+      if (quantiComprada > 0)
+         this.quantiComprada = quantiComprada;
+      else
+         quantiComprada = 0;
+   } // end set quantia
+     // get qusntia
+
+   public int getQuantia() {
+      return quantiComprada;
+   } // get quantis
+
+   // set preço
+   public void setPreco(double preco) {
+      if (preco > 0.0)
+         this.preco = preco;
+      else
+         preco = 0.0;
+   } // end preço
+
+   // get preço
+   public double getPreco() {
+      return preco;
+   } // end preço
+
+   public void status() {
+      System.out.printf("\nFatura: %s%nDescrição: %s%nQuantidade: %d%nPreço R$: %.2f%n",
+            this.getFatura(), // num fatura
+            this.getDiscricao(),
+            this.getQuantia(),
+            this.getPreco());
+      // exibe total da fatura
+      System.out.printf("Total R$: %.2f%n%n", getInvoiceAmount());
+   } // end status
 
 } // end class
