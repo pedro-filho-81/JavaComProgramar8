55497d45ff3fe3d289b1ec3774537e147c1ec4e3
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-22 15:48:12 -0300

    Update EmployeeTest.java

--- livroComoProgramar8/capitulo03/exercicios/employee/EmployeeTest.java
+++ livroComoProgramar8/capitulo03/exercicios/employee/EmployeeTest.java
@@ -1,19 +1,16 @@
 package capitulo03.exercicios.employee;
 
-import java.util.Scanner;
-
 public class EmployeeTest {
    public static void main(String[] args) {
-      // cria o objeto Scanner input
-      Scanner input = new Scanner(System.in);
 
       // cria o objeto da class Employee
       ClasseEmployee myEmployee1 = new ClasseEmployee("Pedro", "Filho", 1650.25);
+
       // cria o objeto myEmployee2 da classe Employee
       ClasseEmployee myEmployee2 = new ClasseEmployee("Cleonice", "morais", 2125.25);
 
       // exibe
-      myEmployee1.status();
-      myEmployee2.status();
+      myEmployee1.status(); // empregado 1
+      myEmployee2.status(); // empregado 2
    } // end main
 } // end class
