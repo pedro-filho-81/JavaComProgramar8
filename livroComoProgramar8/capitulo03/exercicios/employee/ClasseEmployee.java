9da7d37bd49c610f10a6d6f88bcf4f25c82628a8
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-22 15:18:41 -0300

    Update ClasseEmployee.java

--- livroComoProgramar8/capitulo03/exercicios/employee/ClasseEmployee.java
+++ livroComoProgramar8/capitulo03/exercicios/employee/ClasseEmployee.java
@@ -28,8 +28,23 @@ public class ClasseEmployee {
    public ClasseEmployee(String nome, String sobreNome, double salarioMes) {
       this.nome = nome;
       this.sobreNome = sobreNome;
-      this.salarioMes = salarioMes;
+
+      // this.salarioMes = salarioMes;
+      this.setSalarioMes(salarioMes);
    } // end construct
+
+   // método para calcular o salário anual
+   private double salarioAnual() {
+      double salarioAnual = this.getSalarioMes() * 12;
+      return salarioAnual;
+   } // end salarioanual
+
+   // método para calcular o aumento de 10%
+   // sobre o salário anual
+   private double aumento() {
+      // retorna o salário com 10% de aumento
+      return salarioAnual() * 0.10;
+   } // end aumento
 
    // setNome
    public void setNome(String nome) {
@@ -53,12 +68,29 @@ public class ClasseEmployee {
 
    // setSalario
    public void setSalarioMes(double salarioMes) {
-      this.salarioMes = salarioMes;
+      if (salarioMes > 0.0)
+         this.salarioMes = salarioMes;
    } // end setSalarioMes
+
+   // getSalarioMes
+   public double getSalarioMes() {
+      return salarioMes;
+   } // end getSalarioMes
 
    // cria método status
    public void status() {
       System.out.println("\nNovo Empregdo:");
-      System.out.printf("Nome: %s%nSobreNome: %s%nSalário R$: %.2f%n%n", this.nome, this.sobreNome, this.salarioMes);
+      System.out.printf("Nome: %s%nSobreNome: %s%nSalário mensal: R$ %.2f%n",
+            this.getNome(),
+            this.getSobreNome(),
+            this.getSalarioMes());
+
+      // exibe o salario anual
+      System.out.printf("Salário anual: R$ %.2f%n", salarioAnual());
+      // exibe aumento de 10%
+      System.out.printf("Aumento de 10%%: R$ %.2f%n", aumento());
+      // exibe salário anual com o aumento de 10%
+      System.out.printf("Salário anual: R$ %.2f%n", (salarioAnual() + aumento()));
+
    } // end status
 } // end class
