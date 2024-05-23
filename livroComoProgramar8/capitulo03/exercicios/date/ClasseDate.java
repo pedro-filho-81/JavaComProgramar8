7aea1701420b8bd1be2da9090802cee5b51c85bf
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-23 08:33:59 -0300

    Update ClasseDate.java

--- livroComoProgramar8/capitulo03/exercicios/date/ClasseDate.java
+++ livroComoProgramar8/capitulo03/exercicios/date/ClasseDate.java
@@ -1,5 +1,16 @@
 package capitulo03.exercicios.date;
 
+/*
+ * 3.14 (Classe Date) Crie uma classe chamada Date que inclua três 
+ * variáveis de instância — mês (tipo int), dia (tipo int) e ano 
+ * (tipo int). Forneça um construtor que inicializa as três 
+ * variáveis de instância supondo que os valores fornecidos 
+ * estejam corretos. Ofereça um método 
+ * set e um get para cada variável de instância. Apresente um 
+ * método displayDate que exiba mês, dia e ano separados por 
+ * barras normais (/). Escreva um aplicativo de teste chamado 
+ * DateTest que demonstre as capacidades da classe Date.
+ */
 public class ClasseDate {
 
    // variáveis de instância
