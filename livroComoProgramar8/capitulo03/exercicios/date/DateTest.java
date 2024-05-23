33f3314e828df5c5447d63eab70bb4c4c3029571
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-23 07:56:29 -0300

    Create DateTest.java

---
+++ livroComoProgramar8/capitulo03/exercicios/date/DateTest.java
@@ -0,0 +1,16 @@
+package capitulo03.exercicios.date;
+
+public class DateTest {
+   public static void main(String[] args) {
+
+      // cria o objeto da classeDate data1
+      ClasseDate data1 = new ClasseDate(01, 05, 2024);
+
+      // objeto data2
+      ClasseDate data2 = new ClasseDate(04, 05, 2024);
+
+      // exibir
+      data1.displayDate();
+      data2.displayDate();
+   } // end main
+} // end class
