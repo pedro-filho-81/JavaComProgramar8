667ef6bd95912784d78b6fad9c63dc3cf82506bc
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-23 14:51:12 -0300

    Create Student.java

---
+++ livroComoProgramar8/capitulo04/exemplos/students/Student.java
@@ -0,0 +1,64 @@
+package capitulo04.exemplos.students;
+
+/*
+ * Classe Student armazena o nome e calcula a média das notas de um aluno
+ */
+public class Student {
+
+   // variáveis de instncia
+   private String name;
+   private double average;
+
+   // construtor
+   public Student(String name, double average) {
+
+      this.name = name;
+
+      // valida a média é > 0.0 e <= 100; caso contrário
+      // armazena o valor padrão da média da variável de instância (0.0)
+      if (average > 0.0)
+         if (average <= 100)
+            this.average = average;
+   } // construct
+
+   // getLetterGrade
+   public String getLetterGrade() {
+
+      // inicializa com a string vazia
+      String letterGrade = "";
+
+      // verifica o valor da nota
+      if (average >= 90)
+         letterGrade = "A";
+      else if (average >= 80)
+         letterGrade = "B";
+      else if (average >= 70)
+         letterGrade = "C";
+      else if (average >= 60)
+         letterGrade = "D";
+      else
+         letterGrade = "F";
+
+      return letterGrade;
+   } // end getLetterGrade
+
+   public void setName(String name) {
+      this.name = name;
+   } // end setName
+
+   public String getName() {
+      return name;
+   } // end getName
+
+   // setAverage
+   public void setAverage(double average) {
+      if (average > 0.0)
+         if (average <= 100)
+            this.average = average;
+   } // end setAverage
+
+   public double getAverage() {
+      return average;
+   } // end getAverage
+
+} // end class
