122362227d6d0af60b4d4d780eb1aef8d324f0a6
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-31 11:58:17 -0300

    Create Welcome1.java

---
+++ livroComoProgramar8/capitulo02/exemplos/Welcome1.java
@@ -0,0 +1,11 @@
+/*
+   Programa que imprime texto na tela
+ */
+public class Welcome1 {
+   // método maininicia e execução do aplicativo java
+   public static void main(String[] args) {
+
+      // mostra o texto na tela
+      System.out.println("Welcome to java programming");
+   } // end main
+} // end class
