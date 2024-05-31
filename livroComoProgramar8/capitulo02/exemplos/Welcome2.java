3b6ae054b76466886812608d9386d398f848192a
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-31 11:58:23 -0300

    Create Welcome2.java

---
+++ livroComoProgramar8/capitulo02/exemplos/Welcome2.java
@@ -0,0 +1,11 @@
+/*
+ * Imprimindo uma linha e texto com multiplas instruções
+ */
+public class Welcome2 {
+   public static void main(String[] args) {
+
+      // print result
+      System.out.print("Welcome to ");
+      System.out.println("Java programming.");
+   } // end main
+} // end class
