7b1426f1e23687ad345a7e0e2dbc625f209a3caa
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-05-31 11:58:30 -0300

    Create Welcome3.java

---
+++ livroComoProgramar8/capitulo02/exemplos/Welcome3.java
@@ -0,0 +1,10 @@
+/*
+ * Imprimindo multiplas linhas de texto com uma única instrução
+ */
+public class Welcome3 {
+   public static void main(String[] args) {
+
+      // print result
+      System.out.println("Welcome\nto\nJava\nprogramming!");
+   } // end main
+} // end class
