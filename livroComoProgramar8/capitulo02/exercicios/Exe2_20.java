5bd0a4c0cd6de5a70e09ba4b6a5abc92812de67e
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-01 07:49:27 -0300

    Create Exe2_20.java

---
+++ livroComoProgramar8/capitulo02/exercicios/Exe2_20.java
@@ -0,0 +1,13 @@
+package capitulo02.exercicios;
+
+public class Exe2_20 {
+
+   public static void main(String[] args) {
+      // O que o seguinte código imprime?
+      System.out.println("*");
+      System.out.println("***");
+      System.out.println("*****");
+      System.out.println("****");
+      System.out.println("**");
+   } // end main
+} // end class
