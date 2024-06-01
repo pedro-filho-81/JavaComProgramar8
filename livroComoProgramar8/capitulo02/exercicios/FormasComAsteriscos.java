c37c172b9d6039a0dd49c06cabd09404ef9dd55b
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-01 07:50:03 -0300

    Create FormasComAsteriscos.java

---
+++ livroComoProgramar8/capitulo02/exercicios/FormasComAsteriscos.java
@@ -0,0 +1,15 @@
+package capitulo02.exercicios;
+
+public class FormasComAsteriscos {
+   public static void main(String[] args) {
+      System.out.println("*********      ***       *        *");
+      System.out.println("*       *     *   *     ***      * *");
+      System.out.println("*       *    *     *   *****    *   *");
+      System.out.println("*       *    *     *     *     *     *");
+      System.out.println("*       *    *     *     *    *       *");
+      System.out.println("*       *    *     *     *     *     *");
+      System.out.println("*       *    *     *     *      *   *");
+      System.out.println("*       *     *   *      *       * *");
+      System.out.println("*********      ***       *        *");
+   } // end main
+} // end class
