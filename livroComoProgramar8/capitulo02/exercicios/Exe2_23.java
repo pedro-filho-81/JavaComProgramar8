68257a7193ee391b88964b722acb80c8d5e898fe
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-01 07:49:55 -0300

    Create Exe2_23.java

---
+++ livroComoProgramar8/capitulo02/exercicios/Exe2_23.java
@@ -0,0 +1,8 @@
+package capitulo02.exercicios;
+
+public class Exe2_23 {
+   public static void main(String[] args) {
+      // 2.23 O que o seguinte código imprime?
+      System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+   }
+}
