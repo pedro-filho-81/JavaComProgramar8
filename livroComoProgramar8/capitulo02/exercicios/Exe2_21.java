9d021fcd3a05b8df0702bd57930d4ba0c323c083
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-01 07:49:33 -0300

    Create Exe2_21.java

---
+++ livroComoProgramar8/capitulo02/exercicios/Exe2_21.java
@@ -0,0 +1,12 @@
+package capitulo02.exercicios;
+
+public class Exe2_21 {
+   public static void main(String[] args) {
+      // 2.21 O que o seguinte código imprime?
+      System.out.print("*");
+      System.out.print("***");
+      System.out.print("*****");
+      System.out.print("****");
+      System.out.println("**");
+   } // end main
+} // end class
