c802f18ea59fd05939e1fc96c66408d4c11e35f7
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-01 07:49:21 -0300

    Create Exe2_19.java

---
+++ livroComoProgramar8/capitulo02/exercicios/Exe2_19.java
@@ -0,0 +1,9 @@
+package capitulo02.exercicios;
+
+public class Exe2_19 {
+   public static void main(String[] args) {
+      // o que vai mostrar?
+      System.out.println("O que o código abaixo imprime? ");
+      System.out.printf("*%n**%n***%n****%n*****%n");
+   } // end main
+} // end class
