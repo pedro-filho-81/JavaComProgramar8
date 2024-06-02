c7fe6f97e17850973be781695fbf03fdd1bc2459
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-02 08:34:21 -0300

    Create TabuleiroDeAsterisco.java

---
+++ livroComoProgramar8/capitulo02/exercicios/TabuleiroDeAsterisco.java
@@ -0,0 +1,14 @@
+package capitulo02.exercicios;
+
+public class TabuleiroDeAsterisco {
+   public static void main(String[] args) {
+      System.out.println("* * * * * * * *");
+      System.out.println(" * * * * * * * *");
+      System.out.println("* * * * * * * *");
+      System.out.println(" * * * * * * * *");
+      System.out.println("* * * * * * * *");
+      System.out.println(" * * * * * * * *");
+      System.out.println("* * * * * * * *");
+      System.out.println(" * * * * * * * *");
+   } // end main
+} // end class
