231606ef68ee218acc01a58be6e064cd07a78ba8
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-02 08:34:37 -0300

    Create ValorIntDoChar.java

---
+++ livroComoProgramar8/capitulo02/exercicios/ValorIntDoChar.java
@@ -0,0 +1,11 @@
+package capitulo02.exercicios;
+
+public class ValorIntDoChar {
+   public static void main(String[] args) {
+
+      char letra = 'd';
+
+      // mostra o valor inteiro da letra
+      System.out.printf("O valor de %c é %d%n", letra, (int) letra);
+   }
+}
