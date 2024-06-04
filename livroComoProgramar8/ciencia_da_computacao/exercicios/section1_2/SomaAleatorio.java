03bbaa06b7df8b653d7aa1647252c45328f47092
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-04 08:27:31 -0300

    Update SomaAleatorio.java

--- livroComoProgramar8/ciencia_da_computacao/exercicios/section1_2/SomaAleatorio.java
+++ livroComoProgramar8/ciencia_da_computacao/exercicios/section1_2/SomaAleatorio.java
@@ -1,7 +1,12 @@
 package ciencia_da_computacao.exercicios.section1_2;
 
 /*
- * 
+ * 1.2.20 Escreva um programa que imprima a soma 
+ * de dois números inteiros aleatórios entre 1 e 6 
+ * (como você obteria ao jogar dados). .0fSedgewick, 
+ * Robert; Wayne, Kevin. Ciência da Computação: Uma 
+ * Abordagem Interdisciplinar (p. 46). 
+ * Educação Pearson. Edição do Kindle.
  */
 public class SomaAleatorio {
    public static void main(String[] args) {
@@ -12,6 +17,7 @@ public class SomaAleatorio {
       double alea2 = (1 + Math.random() * 6);
 
       // result
-      System.out.printf("A soma de %.0f + %.0f é %.0f%n", alea1, alea2, alea1 + alea2);
+      System.out.printf("A soma de %.0f + %.0f é %.0f%n",
+            alea1, alea2, alea1 + alea2);
    } // end main
 } // end class
