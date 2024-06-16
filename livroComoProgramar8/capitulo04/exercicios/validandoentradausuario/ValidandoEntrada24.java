dd0c08b2ba7634774fc81fae07cb8ac8365002fd
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-16 15:28:03 -0300

    Update ValidandoEntrada24.java

--- livroComoProgramar8/capitulo04/exercicios/validandoentradausuario/ValidandoEntrada24.java
+++ livroComoProgramar8/capitulo04/exercicios/validandoentradausuario/ValidandoEntrada24.java
@@ -15,23 +15,46 @@ public class ValidandoEntrada24 {
 
       // variável
       int valor = 0;
+      int count1 = 0;
+      int count2 = 0;
+      int count_erro = 0;
+      int soma = 0;
 
       while (valor != -1) {
 
          // entrada do usuário
          System.out.print("Digite o valor 1 ou 2 e (-1 para sair): ");
          valor = input.nextInt();
-
          if (valor == -1) {
             break;
-         } else if (valor != 1) {
-            if (valor != 2) {
-               System.out.println("ERRADO! Você digitou: " + valor);
-               continue;
-            }
-         } // end else
+         } // end break
+           // se valor igual a 1
+         if (valor == 1) {
+            // conte 1
+            count1++;
+         } else
+         // se valor igual a 2
+         if (valor == 2) {
+            // conte 2
+            count2++;
+         } else { // se não
+            // conte erro
+            count_erro++;
+            // imprima
+            System.out.println("ERRADO! Você digitou: " + valor);
+            continue; // continue
+         } // end else if
+         soma = count1 + count2 + count_erro;
+         // exiba o resultado
          System.out.println("Você digitou o valor: " + valor);
       } // end while
+
+      // exibe o resultado
+      System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-");
+      System.out.printf("você digitou %d números 1:\n", count1);
+      System.out.printf("Digitou %d números 2:\n", count2);
+      System.out.printf("Números errados: %d\n", count_erro);
+      System.out.printf("Total de números digitados: %d\n", soma);
       System.out.println("Volte sempre!");
    } // end main
 } // end class
