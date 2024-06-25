0a3fad02cd92e662430e739c4657c700dc3ff166
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-25 18:16:17 -0300

    Create DecimalParaBinario31.java

---
+++ livroComoProgramar8/capitulo04/exercicios/decimal_para_binario/DecimalParaBinario31.java
@@ -0,0 +1,14 @@
+package exercicios.decimal_para_binario;
+
+import java.util.Scanner;
+
+public class DecimalParaBinario31 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Converte o valor Binário em Decimal.");
+        // entrada de dados
+        System.out.print("Informe um valor binário: ");
+        int binario = input.nextInt();
+    } // end main
+} // end class
