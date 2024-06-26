3c933018376be2a1d25b2915499a1762eab287d2
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-26 17:36:34 -0300

    Create DecimalParaBinario31_2.java

---
+++ livroComoProgramar8/capitulo04/exercicios/decimal_para_binario/DecimalParaBinario31_2.java
@@ -0,0 +1,55 @@
+package exercicios.decimal_para_binario;
+
+import java.util.Scanner;
+
+public class DecimalParaBinario31_2 {
+    public static void main(String[] args) {
+
+        // objeto Scanner
+        Scanner input = new Scanner(System.in);
+
+        // variáveis
+        int numeroBinario = 0;
+        int numeroDecimal = 0;
+        int power = 0;
+        int temp = 0;
+
+        // entrada de dados
+        System.out.println("Converte valor Binário em Decimal");
+        System.out.print("Informe o valor binário (-1 para sair): ");
+        // entrada do usuário
+        numeroBinario = input.nextInt();
+        if (numeroBinario != -1) {
+            System.out.printf("O valor binário %d%n", numeroBinario);
+        } // end if
+
+        while (numeroBinario != -1) {
+
+            // enquanto número binário maior que zero faça
+            while (numeroBinario > 0) {
+                // pegando o dígito mais à direita do númerobinário
+                temp = numeroBinario % 10;
+                // agora multiplicando o dígito e adicionando-o
+                // à variável decimalNumber
+                numeroDecimal += temp * Math.pow(2, power);
+                // removendo o dígito mais à direita da variável numeroBinario
+                numeroBinario = numeroBinario / 10;
+                // incrementando a variável de potência em 1 para ser usada como potência para 2
+                power++;
+            } // end while
+              // exibe resultado
+            System.out.printf("Equivale ao valor decimal: %d%n", numeroDecimal);
+            power = 0;
+            numeroDecimal = 0;
+
+            // entrada de dados
+            System.out.println("\nConverte valor Binário em Decimal");
+            System.out.print("Informe o valor binário (-1 para sair): ");
+            // entrada do usuário
+            numeroBinario = input.nextInt();
+            if (numeroBinario != -1) {
+                System.out.printf("O valor binário %d%n", numeroBinario);
+            } // end if
+        } // end while -1
+    } // end main
+} // end class
