70da11ac92f001d048e4d0d665ec6d801d309e62
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-26 17:36:13 -0300

    Update DecimalParaBinario31.java

--- livroComoProgramar8/capitulo04/exercicios/decimal_para_binario/DecimalParaBinario31.java
+++ livroComoProgramar8/capitulo04/exercicios/decimal_para_binario/DecimalParaBinario31.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class DecimalParaBinario31 {
     public static void main(String[] args) {
+        // objeto Scanner
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Converte o valor Binário em Decimal.");
+        // cabeçalho
+        System.out.println("\nConverte o valor Binário em Decimal.");
         // entrada de dados
         System.out.print("Informe um valor binário: ");
-        int binario = input.nextInt();
+        // entrada do usuário
+        String numeroBinario = input.nextLine();
+
+        // converte o valor binário em decimal
+        int numeroDecimal = Integer.parseInt(numeroBinario, 2);
+
+        // exibe os valores
+        System.out.printf("O valor em binário: %s equivale ao número decimal: %d%n",
+                numeroBinario, numeroDecimal);
     } // end main
 } // end class
