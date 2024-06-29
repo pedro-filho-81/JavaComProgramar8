afe512188375e053b0a815184d1d698f5be9344a
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2024-06-29 18:00:25 -0300

    Create ForCounter2.java

---
+++ livroComoProgramar8/capitulo05/exemplos/for_counter/ForCounter2.java
@@ -0,0 +1,14 @@
+/*
+ * Repetição controlado por contador com a instrução for
+ */
+package exemplos.for_counter;
+
+public class ForCounter2 {
+    public static void main(String[] args) {
+        // loop for para contar de 1 a 10
+        for(int count = 1; count <= 10; count++) {
+            // imprime os valores
+            System.out.printf("%d ", count);
+        } // end for
+    } // end main
+} // end class
