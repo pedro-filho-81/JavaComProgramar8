26ca577c9a0f5b548e6e25c0123051d5b9fc17f3
PedroFilho <psfilho81@hotmail.com>
2024-07-01 17:11:50 -0300

    Create DoWhileTeste6.java

---
+++ livroComoProgramar8/capitulo05/exemplos/do_while/DoWhileTeste6.java
@@ -0,0 +1,16 @@
+package exemplos.do_while;
+
+public class DoWhileTeste6 {
+    public static void main(String[] args) {
+        
+        // variável
+        int count = 1;
+        do { // faça
+            // imprima
+            System.out.printf("%d ", count);
+            // incremente 1
+            count++;
+            // enquanto count for menor ou igual a 10
+        } while(count <= 10); // end do while
+    } // end main
+} //end class
