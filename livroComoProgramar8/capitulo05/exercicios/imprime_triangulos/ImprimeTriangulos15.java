256e468886904ceac8d5816c557926250c07b7d7
PedroFilho <psfilho81@hotmail.com>
2024-07-05 17:47:55 -0300

    Update ImprimeTriangulos15.java

--- livroComoProgramar8/capitulo05/exercicios/imprime_triangulos/ImprimeTriangulos15.java
+++ livroComoProgramar8/capitulo05/exercicios/imprime_triangulos/ImprimeTriangulos15.java
@@ -10,27 +10,66 @@
  * Não deve haver outras instruções de saída no programa. 
  * [Dica: os dois últimos padrões requerem que cada linha inicie com um número 
  * adequado de espaços em branco.]
- */
+ * */
 package exercicios.imprime_triangulos;
 
 public class ImprimeTriangulos15 {
     public static void main(String[] args) {
         
+        // cabeçalho
+        System.out.printf("%n%s%16s%15s%16s%n", "(a)", "(b)", "(c)", "(d)");
+
         // loop for controlador
         for(int controle = 0; controle < 10; controle++) {
-            // primeiro triangulo ponta para cima
+            
+            // triangulo (a)
             for(int a = 0; a <= controle; a++) {
                 System.out.print("*");
             } // end for 1
+            
             // triângulo vazio ponta para baixo
-            for(int b = 10; b > controle; b--) {
-                System.out.print("a");
+            for(int b = 15; b > controle; b--) {
+                System.out.print(" ");
             } // end for 2
-            // segundo triângulo ponta ara baixo
+            
+            // triângulo (b)
             for(int b = 10; b > controle; b--) {
                 System.out.print("*");
             } // end for 2
-            System.out.println(""); // pula linha
+            
+            // triângulo vazio ponta para cima
+            for(int c = 0; c <= controle + 3; c++) {
+                System.out.print(" ");
+            } // end for
+            // triângulo vazio
+            for(int i = controle; i >= 0; i--) {
+                System.out.print(" ");
+            } // end for vazio
+            
+            // triângulo (c)
+            for(int i = 10; i > controle; i--) {
+                System.out.print("*");
+            } // end for
+
+            // triângulo vazio ponta para cima
+            for(int c = 0; c < controle; c++) {
+                System.out.print(" ");
+            } // end for
+            // triângulo vazio
+            for(int i = 10; i > controle; i--) {
+                System.out.print(" ");
+            } // end for vazio
+            // triângulo vazio
+            for(int i = 10; i > controle; i--) {
+                System.out.print(" ");
+            } // end for vazio
+            
+            // triângulo (d)
+            for(int i = 0; i <= controle; i++) {
+                System.out.print("*");
+            } // end for
+
+            System.out.println(); // pula linha
         } // end for controle
     } // end  main
 } // end class
