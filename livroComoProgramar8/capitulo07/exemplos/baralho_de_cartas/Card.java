7a757cb0bdba9c104428322cc33746b17dff961f
PedroFilho <psfilho81@hotmail.com>
2024-07-09 11:20:35 -0300

    Create Card.java

---
+++ livroComoProgramar8/capitulo07/exemplos/baralho_de_cartas/Card.java
@@ -0,0 +1,20 @@
+package baralho_de_cartas;
+
+public class Card {
+    
+    // cria variáveis de instância da classe
+    private final String face; // face das cartas
+    private final String suit; // naipe das cartas
+
+    // Construtor de dois argumentos inicializa face e naipe da carta
+    public Card(String cardFace, String cardSuit) {
+        this.face = cardFace;
+        this.suit = cardSuit;
+    } // end construtor
+
+    // retorna representação String do Card
+    public String toString() {
+        return face + "to" + suit;
+    } // end tostring
+    
+} // end class Card
