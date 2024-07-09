f0e845f79cec3b5a43f4808bd360b0c2666b4e12
PedroFilho <psfilho81@hotmail.com>
2024-07-09 10:55:28 -0300

    Create BarChar05.java

---
+++ livroComoProgramar8/capitulo07/exemplos/grafico_de_barras/BarChar05.java
@@ -0,0 +1,36 @@
+package grafico_de_barras;
+public class BarChar05 {
+   /*
+    * Programa de impressão de barras de asteriscos
+    */
+   public static void main(String[] args) {
+
+      // array
+      int[] array = { 7, 0, 5, 0, 3, 0, 1, 2, 4, 2, 1 };
+
+      // print array
+      System.out.print("int[] array = {");
+      for (int counter = 0; counter < array.length; counter++)
+         System.out.print(array[counter] + " ");
+      System.out.println("};");
+
+      // header
+      System.out.println("Grade Distribution");
+
+      // para cada elemento de array, gera saída de barras do gráfico.
+      for (int counter = 0; counter < array.length; counter++) {
+         // gera saída do rótulo de barra ("00-09", ...., "90-99", "100")
+         if (counter == 10)
+            System.out.printf("%5d", 100);
+         else
+            System.out.printf("%02d-%02d", counter * 10, counter * 10 + 9);
+
+         // imprime barra de asteriscos do valor do elemento do array
+         for (int star = 0; star < array[counter]; star++) {
+            System.out.print("*");
+         }
+         // new line
+         System.out.println();
+      } // end for
+   } // end main
+} // end class
